3110e8e349cfcf079eb853c83873603ddf09148b
bogle <bogle@localhost>
2013-01-11 15:20:53 -0500

    Everything all cleaned up and building!

--- PhraseTranslationHelper/UNSQuestionsDialog.cs
+++ PhraseTranslationHelper/UNSQuestionsDialog.cs
@@ -17,7 +17,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using SIL.Utils;
