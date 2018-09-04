f884ea4441b55a229b121f4451017506b2c90640
tom_bogle <tom_bogle@sil.org>
2018-09-04 15:44:39 -0400

    Added new file I accidentally overlooked

---
+++ TxlCore/SectionInfo.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------------------------------------
+#region // Copyright (c) 2018, SIL International.
+// <copyright from='2018' to='2018' company='SIL International'>
+//		Copyright (c) 201, SIL International.
+//
+//		Distributable under the terms of the MIT License (http://sil.mit-license.org/)
+// </copyright>
+#endregion
+//
+// File: SectionInfo.cs
+// ---------------------------------------------------------------------------------------------
+
+namespace SIL.Transcelerator
+{
+	public class SectionInfo
+	{
+		public string Heading { get; }
+
+		public int StartRef { get; }
+
+		public int EndRef { get; }
+
+		public SectionInfo(Section section)
+		{
+			Heading = section.Heading;
+			StartRef = section.StartRef;
+			EndRef = section.EndRef;
+		}
+
+		public override string ToString()
+		{
+			return Heading;
+		}
+	}
+}
