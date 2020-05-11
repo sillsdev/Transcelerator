d7ded6425cab874118395dd37b8cc4d013378163
tom_bogle@sil.org <tom_bogle@sil.org>
2020-05-11 13:04:10 -0400

    Removed unneeded using

--- DataIntegrityTests/TxlQuestionsTests.cs
+++ DataIntegrityTests/TxlQuestionsTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json.Serialization;
 using NUnit.Framework;
 using SIL.Scripture;
 using SIL.Transcelerator;
