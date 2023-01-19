510ef95f8b16e3e31f45776f058cd3e96ff7234b
tombogle <tom_bogle@sil.org>
2023-01-19 17:33:42 -0500

    Small updates to misc libraries

--- Transcelerator/TransceleratorTests/HtmlScriptGeneratorTests.cs
+++ Transcelerator/TransceleratorTests/HtmlScriptGeneratorTests.cs
@@ -1,7 +1,7 @@
 ﻿// ---------------------------------------------------------------------------------------------
-#region // Copyright (c) 2021, SIL International.
-// <copyright from='2021' to='2021' company='SIL International'>
-//		Copyright (c) 2021, SIL International.   
+#region // Copyright (c) 2022, SIL International.
+// <copyright from='2021' to='2022' company='SIL International'>
+//		Copyright (c) 2022, SIL International.   
 //	
 //		Distributable under the terms of the MIT License (http://sil.mit-license.org/)
 // </copyright> 
