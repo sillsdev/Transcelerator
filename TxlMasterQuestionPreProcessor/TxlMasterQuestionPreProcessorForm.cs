afdbdf0fa1daaa99e1311d11266cc466653cd6cf
tombogle <tom_bogle@sil.org>
2023-01-23 18:16:24 -0500

    Changed LocalizationsFileGenerator to omit the Alternatives group if the only alternative is a hidden one (since those do not get localized). This prevents getting empty groups back from Crowdin, which fail the validation check (even though they do no actual harm).
    Corrected some mistakes and added some more questions
    Added/corrected a lot of Spanish translations

--- TxlMasterQuestionPreProcessor/TxlMasterQuestionPreProcessorForm.cs
+++ TxlMasterQuestionPreProcessor/TxlMasterQuestionPreProcessorForm.cs
@@ -1,7 +1,7 @@
 ﻿// ---------------------------------------------------------------------------------------------
-#region // Copyright (c) 2021, SIL International.
-// <copyright from='2018' to='2021' company='SIL International'>
-//		Copyright (c) 2021, SIL International.   
+#region // Copyright (c) 2023, SIL International.
+// <copyright from='2018' to='2023' company='SIL International'>
+//		Copyright (c) 2023, SIL International.   
 //    
 //		Distributable under the terms of the MIT License (http://sil.mit-license.org/)
 // </copyright> 
@@ -15,7 +15,6 @@ using System.IO;
 using System.Text;
 using System.Windows.Forms;
 using Paratext.PluginInterfaces;
-using SIL.Scripture;
 using SIL.Transcelerator;
 using SIL.Transcelerator.Localization;
 using SIL.Utils;
